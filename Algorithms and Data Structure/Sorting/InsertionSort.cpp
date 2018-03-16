9f1750c345128b3b17b6c052d4482be1961d66df
Daniil Lee <danya4og@mail.ru>
2018-03-16 23:24:43 +0300

    Update InsertionSort.cpp

--- Algorithms and Data Structure/Sorting/InsertionSort.cpp
+++ Algorithms and Data Structure/Sorting/InsertionSort.cpp
@@ -1,13 +1,13 @@
-void InsertionSort(int arr[], int size){
-	for (int i = 0; i < size; i++){
-
+void InsertionSort(int arr[], int size) {
+	for (int i = 0; i < size; i++) {
 		int j = 0;
 
 		while (arr[j] < arr[i])
 			j++;
 
 		int temp = arr[i];
-		for (int k = i; k > j; k--){
+		
+		for (int k = i; k > j; k--) {
 			arr[k] = arr[k-1];
 		}
 		arr[j] = temp;
