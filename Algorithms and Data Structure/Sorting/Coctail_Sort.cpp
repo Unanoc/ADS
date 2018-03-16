8773df4c1ec028c4c9759a2e0ec5f75fd4b79a7a
Daniil Lee <danya4og@mail.ru>
2018-03-16 23:22:53 +0300

    Update Coctail_Sort.cpp

--- Algorithms and Data Structure/Sorting/Coctail_Sort.cpp
+++ Algorithms and Data Structure/Sorting/Coctail_Sort.cpp
@@ -2,16 +2,16 @@ void CoctailSort(int a[], int n) {
     int left = 0;
     int right = n;
 
-    while (left <= right){
+    while (left <= right) {
 
-        for (int i = left; i < right - 1; i++){
+        for (int i = left; i < right - 1; i++) {
             if (a[i] > a[i+1])
                 swap(a[i],a[i+1]);
         }
 
         right--;
 
-        for (int i = right; i >= left + 1; i-- ){
+        for (int i = right; i >= left + 1; i-- ) {
             if (a[i] < a[i-1])
                 swap(a[i], a[i-1]);
         }
