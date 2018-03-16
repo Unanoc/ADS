85842f5f7b70bcb8a424f1ce375aef1c7995eb5f
Daniil Lee <danya4og@mail.ru>
2018-03-16 23:27:00 +0300

    Update SelectionSort.cpp

--- Algorithms and Data Structure/Sorting/SelectionSort.cpp
+++ Algorithms and Data Structure/Sorting/SelectionSort.cpp
@@ -1,14 +1,14 @@
-void SelectionSort(int arr[], int size){
+void SelectionSort(int arr[], int size) {
     int temp, minIndex;
 
-    for (int i = 0; i < size; i++){
+    for (int i = 0; i < size; i++) {
         minIndex = i;
 
-        for (int j = i + 1; j < size; j++){
+        for (int j = i + 1; j < size; j++) {
             if (arr[j] < arr[i])
                 minIndex = j;
         }
-        if (minIndex != i){
+        if (minIndex != i) {
             temp = arr[i];
             arr[i] = arr[minIndex];
             arr[minIndex] = temp;
